9a33f9d21061b125a9bb7230829107d3d9090c2d
lilvina <davinaleo2007@yahoo.com>
2019-09-25 00:29:41 -0700

    working on beach resort app

--- beach-resort/src/pages/Home.js
+++ beach-resort/src/pages/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
-
-const Home = () => {
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom'
+export default function Home() {
   return(
-    <div>
-      Hello from home page
-    </div>
+    <Hero>
+      <Banner title="luxurious rooms"
+      subtitle="deluxe rooms starting at $299">
+        <Link to="/rooms" className="btn-primary">
+          Our rooms
+        </Link>
+      </Banner>
+    </Hero>
   )
 }
-
-export default Home;
