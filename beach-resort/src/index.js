94a5455f31b10fb7e6d8de3e2adf2f33ca6bb5b4
lilvina <davinaleo2007@yahoo.com>
2019-09-25 15:43:02 -0700

    react beach app is coming along

--- beach-resort/src/index.js
+++ beach-resort/src/index.js
@@ -4,12 +4,15 @@ import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {RoomProvider} from './context';
 
 ReactDOM.render(
-  <Router>
+  <RoomProvider>
+    <Router>
     <App />
-  </Router>,
-  document.getElementById('root'));
+    </Router>
+  </RoomProvider>,
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
